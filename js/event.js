2cc9cb2192301e9d5ef356a693ba0e7c037cc9a0
Daniel <danielwinther@hotmail.dk>
2016-04-11 00:38:05 +0200

    notifikationer + dagens ord

--- js/event.js
+++ js/event.js
@@ -1,3 +1,4 @@
+const URL = 'http://ordnet.dk/ddo';
 chrome.runtime.onInstalled.addListener(function() {
 	var id = chrome.contextMenus.create({
 		title: 'Slå \"%s\" op i ordbogen', 
@@ -7,7 +8,43 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 chrome.contextMenus.onClicked.addListener(klikHandler);
 function klikHandler(info, tab) {
-	chrome.tabs.create({ 
-		url: 'popup.html#/?soegetekst=' + info.selectionText,
+
+	$.ajax({
+		url: URL + '/ordbog?query=' + info.selectionText,
+		type: 'GET',
+		success: function(html){ 
+			var ord = new Array();
+			$('div.definitionBoxTop > span.match', html).each(function() {
+				ord.push($(this).text());
+			});
+			var betydning = $(html).find('#betydning-1 > span > span').text();
+			var ordklasse = $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text();
+			
+			var opt = {
+				type: 'basic',
+				title: ord.toString(),
+				message: betydning,
+				contextMessage: ordklasse,
+				iconUrl: 'img/ikon128.png',
+				priority: 0
+			}
+			chrome.notifications.create('opslag', opt);
+		},
+		error: function(html) {
+			var opt = {
+				type: 'basic',
+				title: 'Ingen resultater med \"' + info.selectionText + '\"',
+				message: 'Der blev ikke fundet nogen resultater med søgningen \"' + info.selectionText + '\". Prøv en anden søgetekst.',
+				iconUrl: 'img/ikon128.png',
+				priority: 0
+			}
+			chrome.notifications.create('opslag', opt);
+		}
+	});
+
+	chrome.notifications.onClicked.addListener(function notificationId() {
+		chrome.tabs.create({url: URL + '/ordbog?query=' + info.selectionText}, function tab() {
+			chrome.notifications.clear('opslag');
+		});
 	})
 };
--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 var ordnet = angular.module('ordnet', []);
-ordnet.constant('URL', 'http://ordnet.dk/ddo/ordbog?query=');
+ordnet.constant('URL', 'http://ordnet.dk/ddo');
 ordnet.controller('OrdnetController', function($scope, $http, $location, URL) {
-    $scope.init = function() {
+    $scope.initialiser = function() {
         $scope.manifest = chrome.runtime.getManifest();
         $('[data-toggle="tooltip"]').tooltip(); 
 
@@ -10,36 +10,35 @@ ordnet.controller('OrdnetController', function($scope, $http, $location, URL) {
             $scope.soeg($scope.soegetekst);    
         }
     };
-    $scope.onChange = function() {
+    $scope.vedAendring = function() {
         $scope.soeg($scope.soegetekst);
     };
-    $scope.menteDuClick = function(mente) {
+    $scope.menteDuKlik = function(mente) {
         $scope.soegetekst = mente;  
         $scope.soeg($scope.soegetekst);
     };
     $scope.soeg = function(soegetekst) {
-        $http.get(URL + soegetekst)
+        $http.get(URL + '/ordbog?query=' + soegetekst)
         .then(function(response) {
             var html = response.data;
 
             var ord = new Array();
-            $('div.definitionBoxTop > span.match', html).each(function(index) {
+            $('div.definitionBoxTop > span.match', html).each(function() {
                 ord.push($(this).text());
             });
             $scope.ord = ord;
-            $scope.ordklasse = $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text() ? $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text() : 'Ingen';
+            $scope.ordklasse = $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text() ? $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text() : null;
             var betydninger = new Array();
-            $('#content-betydninger .definitionBox > .tekstmedium > .definition', html).each(function(index) {
+            $('#content-betydninger .definitionBox > .tekstmedium > .definition', html).each(function() {
                 betydninger.push($(this).text());
             });
             $scope.betydninger = betydninger;
             $scope.boejning = $(html).find('#id-boj > span.tekstmedium.allow-glossing').text() ? $(html).find('#id-boj > span.tekstmedium.allow-glossing').text() : null;
             $scope.udtale = $(html).find('#id-udt > span.tekstmedium.allow-glossing > span').text() ? $(html).find('#id-udt > span.tekstmedium.allow-glossing > span').text() : null;
             $scope.oprindelse = $(html).find('#id-ety > span.tekstmedium.allow-glossing').text() ? $(html).find('#id-ety > span.tekstmedium.allow-glossing').text() : null;
-            ///$scope.synonymer = $(html).find('#content-betydninger > div:nth-child(2) > div.definitionBox.onym > span:nth-child(2)').text() ? $(html).find('#content-betydninger > div:nth-child(2) > div.definitionBox.onym > span:nth-child(2)').text() : 'Ingen';
-            //$scope.antonymer = $(html).find('#content-betydninger > div:nth-child(2) > div.definitionBox.onym > span:nth-child(4)').text() ? $(html).find('#content-betydninger > div:nth-child(2) > div.definitionBox.onym > span:nth-child(4)').text() : 'Ingen';
+            $scope.synonymerOgAntonymer = $(html).find('#content-betydninger > div:nth-child(2) > div.definitionBox.onym').text() ? $(html).find('#content-betydninger > div:nth-child(2) > div.definitionBox.onym').text() : null;
             var menteDu = new Array();
-            $('#alikebox-show-all > a', html).each(function(index) {
+            $('#alikebox-show-all > a', html).each(function() {
                 menteDu.push($(this).text());
             });
             $scope.menteDu = menteDu;
@@ -55,8 +54,17 @@ ordnet.controller('OrdnetController', function($scope, $http, $location, URL) {
         $scope.boejning = fejl;
         $scope.udtale = fejl;
         $scope.oprindelse = fejl;
-        $scope.synonym = fejl;
-        $scope.antonymer = fejl;
+        $scope.synonymerOgAntonymer = fejl;
         $scope.menteDu = fejl;
     }
+    $scope.dagensOrd = function() {
+        $http.get(URL)
+        .then(function(response) {
+            var html = response.data;
+
+            var dagensOrd = $(html).find('.dagensord > .match').text();
+            $scope.soegetekst = dagensOrd;
+            $scope.soeg($scope.soegetekst);
+        });
+    };
 });
