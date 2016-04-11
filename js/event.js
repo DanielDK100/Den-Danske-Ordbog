de9c4ef8e48a499ae5b62ad7d0a2cffa3a08343d
Daniel <danielwinther@hotmail.dk>
2016-04-11 21:58:29 +0200

    opslag med flere ord

--- js/event.js
+++ js/event.js
@@ -1,50 +1,55 @@
 const URL = 'http://ordnet.dk/ddo';
 chrome.runtime.onInstalled.addListener(function() {
 	var id = chrome.contextMenus.create({
-		title: 'Slå \"%s\" op i ordbogen', 
+		title: 'Slå \'%s\' op i ordbogen', 
 		contexts:['selection'],
 		id: 'context' + 'selection'
 	});  
 });
 chrome.contextMenus.onClicked.addListener(klikHandler);
 function klikHandler(info, tab) {
+	var i = 0;
+	var soegetekst = info.selectionText.replace(/\./g, '').replace(/,/g, '').replace(/\//g, ' ');
+	$.map((soegetekst).split(' '), function(nytOrd) {
+		if (i < 5) {
+			$.ajax({
+				url: URL + '/ordbog?query=' + nytOrd,
+				type: 'GET',
+				success: function(html){ 
+					var ord = new Array();
+					$('div.definitionBoxTop > span.match', html).each(function() {
+						ord.push($(this).text());
+					});
+					var betydning = $(html).find('#betydning-1 > span > span').text();
+					var ordklasse = $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text();
 
-	$.ajax({
-		url: URL + '/ordbog?query=' + info.selectionText,
-		type: 'GET',
-		success: function(html){ 
-			var ord = new Array();
-			$('div.definitionBoxTop > span.match', html).each(function() {
-				ord.push($(this).text());
+					var opt = {
+						type: 'basic',
+						title: ord.toString(),
+						message: betydning,
+						contextMessage: ordklasse,
+						iconUrl: 'img/ikon128.png',
+						priority: 0
+					}
+					chrome.notifications.create(nytOrd, opt);
+				},
+				error: function(html) {
+					var opt = {
+						type: 'basic',
+						title: 'Ingen resultater med \'' + nytOrd + '\'',
+						message: 'Der blev ikke fundet nogen resultater med søgningen \'' + nytOrd + '\'. Prøv en anden søgetekst.',
+						iconUrl: 'img/ikon128.png',
+						priority: 0
+					}
+					chrome.notifications.create(nytOrd, opt);
+				}
 			});
-			var betydning = $(html).find('#betydning-1 > span > span').text();
-			var ordklasse = $(html).find('div.definitionBoxTop > span.tekstmedium.allow-glossing').text();
-			
-			var opt = {
-				type: 'basic',
-				title: ord.toString(),
-				message: betydning,
-				contextMessage: ordklasse,
-				iconUrl: 'img/ikon128.png',
-				priority: 0
-			}
-			chrome.notifications.create('opslag', opt);
-		},
-		error: function(html) {
-			var opt = {
-				type: 'basic',
-				title: 'Ingen resultater med \"' + info.selectionText + '\"',
-				message: 'Der blev ikke fundet nogen resultater med søgningen \"' + info.selectionText + '\". Prøv en anden søgetekst.',
-				iconUrl: 'img/ikon128.png',
-				priority: 0
-			}
-			chrome.notifications.create('opslag', opt);
+			chrome.notifications.onClicked.addListener(function notificationId() {
+				chrome.tabs.create({url: URL + '/ordbog?query=' + nytOrd}, function tab() {
+					chrome.notifications.clear(nytOrd);
+				});
+			})
 		}
-	});
-
-	chrome.notifications.onClicked.addListener(function notificationId() {
-		chrome.tabs.create({url: URL + '/ordbog?query=' + info.selectionText}, function tab() {
-			chrome.notifications.clear('opslag');
-		});
+		i++;
 	})
 };
