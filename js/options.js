b6a6d79d5a49d813d6e6b7fc4998b8336e88492e
Daniel <danielwinther@hotmail.dk>
2017-01-30 23:12:40 +0100

    options

--- js/options.js
+++ js/options.js
@@ -1,6 +1,20 @@
 var ordnet = angular.module('ordnet', []);
 ordnet.controller('IndstillingerController', function($scope) {
-    $scope.initialiser = function() {
-        $scope.manifest = chrome.runtime.getManifest();
-    };
+	$scope.initialiser = function() {
+		$scope.manifest = chrome.runtime.getManifest();
+
+		indlaesIndstillinger();
+	};
+
+	$scope.gemIndstillinger = function(vaerdi) {
+		chrome.storage.sync.set({
+			autocomplete: vaerdi
+		});
+	};
+
+	function indlaesIndstillinger(indstilling = null) {
+		chrome.storage.sync.get(indstilling, function(resultat) {
+			$scope.resultat = resultat;
+		});
+	}
 });
