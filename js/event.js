5fa3aafec8b7c6fac4960387d6c3a9808f16061b
Daniel <danielwinther@hotmail.dk>
2017-02-12 19:45:39 +0100

    fjern tal

--- js/event.js
+++ js/event.js
@@ -24,7 +24,7 @@ function klikHandler(info, tab) {
 			.done(function(html) {
 				var html = $(html).filter('.ar')[0];
 
-				opt.title = $(html).find('.head').first().text() ? $(html).find('.head').first().text().trim() : 'Ingen resultater med \"' + nytOrd + '\"';
+				opt.title = $(html).find('.head').first().text() ? $(html).find('.head').first().text().trim().replace(/\d+/g, '') : 'Ingen resultater med \"' + nytOrd + '\"';
 				opt.message = $(html).find('.dtrn').first().text().trim();
 				opt.contextMessage = $(html).find('.pos').first().text().trim();
 			}).always(function(){
