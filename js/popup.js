a362d6a059bc266b25fba4c0846d21768dc67209
Daniel <danielwinther@hotmail.dk>
2017-03-25 03:02:41 +0100

    css

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 var ordbog = angular.module('ordbog', ['ngSanitize']);
 ordbog.controller('OrdController', function($scope) {
     $scope.initialiser = function() {
-        console.log(chrome.i18n.getMessage("popupUdvikling") + ' AngularJS v' + angular.version.full);
+        console.log(chrome.i18n.getMessage("popupUdvikling") + ' AngularJS ' + angular.version.full);
         $scope.placeholder = chrome.i18n.getMessage("popupPlaceholder");
         $scope.manifest = chrome.runtime.getManifest();
         $scope.background = {
