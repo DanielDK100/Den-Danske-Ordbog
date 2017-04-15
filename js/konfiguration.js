17e2f7daa59f4e46ae932560b7a2d6f1439f27d2
Daniel <danielwinther@hotmail.dk>
2017-04-15 23:42:23 +0200

    fix

--- js/konfiguration.js
+++
@@ -1,6 +0,0 @@
-var konfiguration = {
-    'urlWs': 'http://ws.dsl.dk/ddo/query',
-    'urlAutocomplete': 'http://ordnet.dk/ws/ddo/livesearch',
-    'urlDenDanskeOrdbog': 'http://ordnet.dk/ddo/ordbog?query=',
-    'googleAnalytics': 'UA-89703064-2'
-}
