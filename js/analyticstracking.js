5cebe7a1c01809a7a9085e4f4807165e5efd6bae
Daniel <danielwinther@hotmail.dk>
2017-04-16 01:06:09 +0200

    minify

---
+++ js/analyticstracking.js
@@ -0,0 +1,8 @@
+var _gaq = _gaq || [];
+_gaq.push(["_setAccount", konfiguration.googleAnalytics]), _gaq.push(["_trackPageview"]),
+function() {
+	var a = document.createElement("script");
+	a.type = "text/javascript", a.async = !0, a.src = "https://ssl.google-analytics.com/ga.js";
+	var b = document.getElementsByTagName("script")[0];
+	b.parentNode.insertBefore(a, b)
+}();
---
+++ js/autocomplete.js
@@ -0,0 +1,23 @@
+var soegetekst = $('#soegetekst');
+soegetekst.keyup(function(event) {
+    $('html').css('height', 0);
+    $.getJSON(konfiguration.urlAutocomplete, {text: soegetekst.val(), size: 10}, function(response) {
+        soegetekst.autocomplete({
+          source: response,
+          autoFocus: true,
+          classes: {
+            'ui-autocomplete': 'notranslate'
+        },
+        select: function(event, ui) {
+            soegetekst.val(ui.item.value).trigger('input').blur();
+        },
+        open: function(event, ui) {
+            $('.container').css('minHeight', $('.ui-autocomplete').height() + 105);
+        },
+        close: function(event, ui) {
+            $('.container').css('minHeight', 0);
+            soegetekst.focus();
+        }
+    });
+    });
+});
