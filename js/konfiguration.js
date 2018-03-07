beaafbd1227fc06f050968671ba764f76ccb1e92
Daniel Winther Jensen <danielwinther@hotmail.dk>
2018-03-07 21:48:59 +0100

    Update dependencies

--- js/konfiguration.js
+++ js/konfiguration.js
@@ -1,6 +1,6 @@
 var konfiguration = {
-    'urlWs': 'http://ws.dsl.dk/ddo/query',
-    'urlAutocomplete': 'http://ordnet.dk/ws/ddo/livesearch',
-    'urlDenDanskeOrdbog': 'http://ordnet.dk/ddo/ordbog?query=',
-    'googleAnalytics': 'UA-89703064-2'
+	'urlWs': 'http://ws.dsl.dk/ddo/query',
+	'urlAutocomplete': 'http://ordnet.dk/ws/ddo/livesearch',
+	'urlDenDanskeOrdbog': 'http://ordnet.dk/ddo/ordbog?query=',
+	'googleAnalytics': 'UA-89703064-2'
 }
